7c7d0efe00ef50a51ca163cd3a7439ec30f294a9
grabmuel <grabmuel@ebi.ac.uk>
2016-05-13 14:17:14 +0100

    Simplify checking for missing config entries

--- urls.py
+++ urls.py
@@ -6,27 +6,14 @@ class ConfigException(Exception):
     pass
 
 
-def _raise_exception(name: str):
-    raise ConfigException('{0} is not set in urls.cfg'.format(name))
-
-
 _config = configparser.ConfigParser()
 _config.read(os.path.join(os.path.dirname(__file__), 'urls.cfg'))
 
-test_server = _config['ENA submission']['test']
-if not test_server:
-    _raise_exception('ENA submission/test')
-
-production_server = _config['ENA submission']['production']
-if not production_server:
-    _raise_exception('ENA submission/production')
-
-ftp_server = _config['ENA ftp']['host']
-if not ftp_server:
-    _raise_exception('ENA ftp/host')
-
-sqlite = _config['result DB']['sqlite']
-
-arrayexpress = _config['ArrayExpress']['api']
-if not arrayexpress:
-    _raise_exception('ArrayExpress/api')
+try:
+    test_server = _config['ENA submission']['test']
+    production_server = _config['ENA submission']['production']
+    ftp_server = _config['ENA ftp']['host']
+    sqlite = _config['result DB']['sqlite']
+    arrayexpress = _config['ArrayExpress']['api']
+except KeyError as e:
+    raise ConfigException('{0} is not set in urls.cfg'.format(e.args[0])) from None
